2f1a741db4abb7c005e95f55383daf11d14e290b
JesusCT18 <jcoltoc899@g.educaand.es>
2026-03-23 12:31:26 +0000

    Desplegando ruta /metrics para Grafana

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mi App DevOps
+
+## Ejecutar
+npm install
+npm run dev
+
+## Docker
+docker build -t mi-app .
+docker run -p 3000:3000 mi-app
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import client from "prom-client"; // Importamos la librería de métricas
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Usamos el puerto de Render o el 3000
 
+// --- CONFIGURACIÓN DE MÉTRICAS ---
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register });
+
+// Tu middleware de log que ya tenías
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// --- NUEVA RUTA PARA GRAFANA ---
+// Grafana entrará aquí para leer el uso de CPU y RAM
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// Tu ruta principal
 app.get("/", (req, res) => {
-  res.send("App funcionando 🚀");
+  res.send("App funcionando 🚀 - Kryos está vigilando el rendimiento");
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
